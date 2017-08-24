c037b745d4cee051d70c33c86aabd2b99c9f189a
Abe Pazos <abe@hamoid.com>
2017-08-24 12:37:25 +0200

    Example: use keys to load/save. Use range-based for loop

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,100 +1,112 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
     ofSetBackgroundColor(ofColor::black);
-    
+
     bPolyClosed = false;
     polyThickness = 20;
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
     poly.setClosed(bPolyClosed);
-    
+
     ofxPolyGrow(polyOuter, poly, polyThickness);
     ofxPolyGrow(polyInner, poly, -polyThickness);
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 
     poly.draw();
-    
-    const vector<ofVec3f> & points = poly.getVertices();
-    for(int i=0; i<points.size(); i++) {
-        const ofVec3f & point = points[i];
+
+    const auto & points = poly.getVertices();
+    for(size_t i = 0; i < points.size(); i++) {
+        const auto & point = points[i];
         ofDrawCircle(point.x, point.y, 4);
     }
-    
+
     ofxPolyDrawNormals(poly, polyThickness);
-    
+
     ofSetColor(ofColor::magenta);
     polyOuter.draw();
-    
+
     ofSetColor(ofColor::cyan);
     polyInner.draw();
-    
+
     ofSetColor(ofColor::white);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
     if(key == 'c' || key == 'C') {
         bPolyClosed ^= true;
     }
     if(key == 'r' || key == 'R') {
         poly.clear();
     }
+    if(key == 'l' || key == 'L') {
+        if(ofxPolyLoad(poly, "poly.xml")) {
+            bPolyClosed = poly.isClosed();
+            cout << "poly.xml loaded" << endl;
+        } else {
+            cout << "poly.xml not found" << endl;
+        }
+    }
+    if(key == 's' || key == 'S') {
+        ofxPolySave(poly, "poly.xml");
+        cout << "poly.xml saved" << endl;
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y ) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
     poly.addVertex(x, y);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
