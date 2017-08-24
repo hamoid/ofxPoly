624dac82fb5fd042e415f8ab174b7d6cac4987b6
Abe Pazos <abe@hamoid.com>
2017-08-24 12:34:43 +0200

    load/save using pugixml
    Use auto and modern for loop.
    
    Avoid saving attributes to the root node because of this:
    https://forum.openframeworks.cc/t/27585

--- src/ofxPoly.cpp
+++ src/ofxPoly.cpp
@@ -163,43 +163,34 @@ void ofxPolyDrawNormals(const ofPolyline & poly, float normalLength) {
 //--------------------------------------------------------------
 void ofxPolySave(const ofPolyline & poly, string xmlPath) {
     ofXml xml;
-    xml.addChild("poly");
-    xml.setAttribute("closed", ofToString(poly.isClosed()));
-    for(int i=0; i<poly.size(); i++) {
-        const ofPoint & point = poly.getVertices()[i];
-        
-        xml.addChild("point");
-        xml.setToChild(i);
-        xml.setAttribute("x", ofToString(point.x));
-        xml.setAttribute("y", ofToString(point.y));
-        xml.setToParent();
+    ofXml data = xml.appendChild("poly");
+    data.appendChild("closed").set(poly.isClosed());
+    for(auto & point : poly.getVertices()) {
+        ofXml p = data.appendChild("point");
+        p.setAttribute("x", point.x);
+        p.setAttribute("y", point.y);
     }
-    
     xml.save(xmlPath);
 }
 
 //--------------------------------------------------------------
-void ofxPolyLoad(ofPolyline & poly, string xmlPath) {
+bool ofxPolyLoad(ofPolyline & poly, string xmlPath) {
     ofXml xml;
-    bool bLoaded = xml.load(xmlPath);
-    if(bLoaded == false) {
-        return;
-    }
-    
-    xml.setTo("poly");
-    bool bClosed = ofToInt(xml.getAttribute("closed"));
-    
-    poly.clear();
-    
-    int numOfPoints = xml.getNumChildren();
-    for(int i=0; i<numOfPoints; i++) {
-        xml.setToChild(i);
-        float x = ofToFloat(xml.getAttribute("x"));
-        float y = ofToFloat(xml.getAttribute("y"));
+    if(xml.load(xmlPath)) {
+        auto data = xml.getChild("poly");
+        bool bClosed = data.getChild("closed").getBoolValue();
         
-        poly.addVertex(x, y);
+        poly.clear();
+        for(auto & p : data.getChildren("point")) {        
+            float x = p.getAttribute("x").getFloatValue();
+            float y = p.getAttribute("y").getFloatValue();
+            poly.addVertex(x, y);
+        }
+        poly.setClosed(bClosed);
+        return true;
+    } else {
+        return false;
     }
-    
-    poly.setClosed(bClosed);
 }
+    
 
